8716e1b3902cf7f931ab77cda0ccf08a2f3be4ad
watch-me-fly <thi.phoned@gmail.com>
2025-01-02 22:44:05 +0100

    Test : Check HTTP Response

---
+++ SafetyNet/src/test/java/com/safetynet/alertsystem/CheckHTTPResponse.java
@@ -0,0 +1,28 @@
+package com.safetynet.alertsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CheckHTTPResponse {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void checkHTTPResponse() {
+
+        String expectedReturn = "HTTP/1.1 200 OK";
+        String actualReturn = testRestTemplate.getForObject("http://localhost:" + port + "/", String.class);
+
+        assertEquals(expectedReturn, actualReturn);
+    }
+}
